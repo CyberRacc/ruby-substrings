c76aea85c67dda5c153864906565fccd7dee8671
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2024-02-19 17:35:34 +0000

    Made progress but still broken

--- substrings.rb
+++ substrings.rb
@@ -7,9 +7,24 @@
 
 dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
 
-def substrings(word, substrings_array)
-    string_to_array = word.split(' ') # Creates an array of substrings from the given word string. Seperate substrings by spaces.
-    string_to_array.each do |substring|
-      
+def substrings(word, dictionary_array)
+    string_count = {} # Create an empty hash
+    scan_output = []
+    word_array = word.split(' ') # Creates an array of substrings from the given word string. Seperate substrings by spaces.
+
+    word_array.each do |word_from_array|
+        clean_substring = word_from_array.gsub(/[[:punct:]]/, '').downcase # Remove punctuation and downcase the string
+        scan_output.push(clean_substring.scan(word_from_array))
     end
+
+    dictionary_array.each do |dictionary_word| # 
+        puts scan_output
+        string_count[dictionary_word] = scan_output.count(dictionary_word) # Something called the scan method may work here.
+    end
+
+    # Return a hash containing each substring as the key, and how many times it was found as the value.
+    return string_count
 end
+
+puts substrings("below", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
